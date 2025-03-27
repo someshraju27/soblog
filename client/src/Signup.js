bf8c04bb937aeff5534ed1001283da219fdc89b0
someshraju27 <someshraju1527@gmail.com>
2025-03-27 16:47:23 +0530

    Update Signup.js

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -238,38 +238,7 @@ const Signup = () => {
                             className="absolute right-3 top-3 cursor-pointer text-gray-500 hover:text-gray-300"
                             onClick={handlePasswordToggle}
                         >
-                            {showPassword ? (
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    fill="none" 
-                                    viewBox="0 0 24 24" 
-                                    stroke="currentColor" 
-                                    className="w-5 h-5"
-                                >
-                                    <path 
-                                        strokeLinecap="round" 
-                                        strokeLinejoin="round" 
-                                        strokeWidth="2" 
-                                        d="M13.875 18.825A10.97 10.97 0 0112 19c-4.418 0-8.418-2.64-10.125-6.56a1.012 1.012 0 010-.88C3.582 7.64 7.582 5 12 5c1.316 0 2.573.23 3.75.65M15 12a3 3 0 11-6 0 3 3 0 016 0zm0 0L21 21M3 3l6 6"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    fill="none" 
-                                    viewBox="0 0 24 24" 
-                                    stroke="currentColor" 
-                                    className="w-5 h-5"
-                                >
-                                    <path 
-                                        strokeLinecap="round" 
-                                        strokeLinejoin="round" 
-                                        strokeWidth="2" 
-                                        d="M12 4.5c-4.64 0-8.577 3.01-9.964 7.178a1.012 1.012 0 000 .639C3.423 16.49 7.36 19.5 12 19.5c4.64 0 8.577-3.01 9.964-7.178a1.012 1.012 0 000-.639C20.577 7.51 16.64 4.5 12 4.5zm0 11.25a3.75 3.75 0 110-7.5 3.75 3.75 0 010 7.5z"
-                                    />
-                                </svg>
-                            )}
-                        </span>
+                            {showPassword ? "🙈" : "👁️"}                        </span>
                     </div>
                     {errors.password && (
                         <p className="text-red-400 text-sm mt-1">{errors.password}</p>
